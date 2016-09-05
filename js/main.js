654d51613317a72cf54dd2ea12a44d11e9876e7b
ualkotob <umaralkotob@gmail.com>
2016-09-04 22:13:52 -0400

    Added Tutorials
    Added Tutorials
    -Callback Functions
    -Dependancy Injection
    -Functional Programming
    -Two Way Data Binding

--- js/main.js
+++ js/main.js
@@ -224,12 +224,32 @@ app.controller('PageCtrl', function ($scope, $http, $routeParams, $sce) {
                summary: "Inheritance using objects instead of classes."
            },
            {
-               name: 'The \'this\' keyword',
+               name: 'The \'this\' Keyword',
                summary: "It refers to the owner of the function being executed."
            },
            {
                name: 'Function Hoisting',
-               summary: "Function declarations are always moved to the top of the current scope at runtime."
+               summary: "Object declarations are always moved to the top of the current scope at runtime."
+           },
+           {
+               name: 'Callback Functions',
+               summary: "Functions passed in as parameters of other functions."
+           },
+           {
+               name: 'Functions as Data',
+               summary: "Functions are considered data like other variables."
+           },
+           {
+               name: 'Two Way Data Binding',
+               summary: "When a variable can be changed in the UI and in the script."
+           },
+           {
+               name: 'Dependency Injection',
+               summary: "Having all a function's dependancies as parameters."
+           },
+           {
+               name: 'Functional Programming',
+               summary: "All function have the same output for the same input."
            }
        ];
 
